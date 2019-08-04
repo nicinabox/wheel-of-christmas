a01bd0e0de93568f09f1497b4b02157669b1d124
Nic Haynes <nic@nicinabox.com>
2019-08-04 11:45:29 -0500

    Update grid

--- src/components/LetterBoard/index.tsx
+++ src/components/LetterBoard/index.tsx
@@ -13,47 +13,11 @@ interface Props {
   onLetterClick: (index: number) => void;
 }
 
-// const ROW_RANGES = [
-//   [0, 10],
-//   [0, 12],
-//   [0, 12],
-//   [0, 10],
-// ]
-
 const LetterBoard: React.FC<Props> = ({ chars, activeChars, solvedIndexes, onLetterClick }) => {
-  // let tileNum = 0
 
   return (
     <div className="LetterBoard">
-      {/* {ROW_RANGES.map((rowRange, rowIndex) => {
-        const row = puzzleRows[rowIndex]
-        const rowChars = row ? row.split('') : []
-        const [start, end] = rowRange;
-
-        return (
-          <div key={rowIndex} className="LetterBoard-row">
-            {range(start, end).map((_, tileIndex) => {
-              const char = rowChars[tileIndex]
-              tileNum++
-
-              return (
-                <Tile key={tileNum} className={`Tile-${tileNum}`}>
-                  {char ? (
-                    <Letter
-                      letter={char}
-                      isActive={activeChars.includes(char)}
-                      isSolved={solvedIndexes.includes(tileNum)}
-                      onClick={() => onLetterClick(tileNum)}
-                    />
-                  ) : null}
-                </Tile>
-              )
-            })}
-          </div>
-        )
-      })} */}
-
-      {range(1,45).map((tileId, index) => {
+      {range(1,49).map((tileId, index) => {
         const char = chars[index]
 
         return (
