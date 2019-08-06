e872ebffda6dc7137cb7842cea4f1f587f05d503
Nic Haynes <nic@nicinabox.com>
2019-08-05 19:17:59 -0500

    Update fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import './fonts/avenir-heavy.css'
+import './fonts/avenir-next.css'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
