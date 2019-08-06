3b5d9449daf6b34f555e89560684c1c60d311fb2
Nic Haynes <nic@nicinabox.com>
2019-08-05 19:02:01 -0500

    Refactor

--- src/components/ControlBoard/index.tsx
+++ src/components/ControlBoard/index.tsx
@@ -4,6 +4,7 @@ import * as API from '../../interfaces/api'
 import PuzzleBoard from '../PuzzleBoard'
 import PuzzleKey from '../PuzzleKey'
 import Category from '../Category'
+import { getRevealedIndexes } from '../../utils'
 
 import './styles.css'
 
@@ -15,17 +16,10 @@ interface Props {
 const ControlBoard: React.FC<Props> = ({ puzzle, onPuzzleChange }) => {
   const chars = puzzle.text.toUpperCase().split('')
 
-  const initialRevealedIndexes = chars.reduce((acc: number[], c, i) => {
-    if (/[^\w]/.test(c)) {
-      return acc.concat(i)
-    }
-    return acc
-  }, [])
-
   const [shouldPopOut, setShouldPopOut] = useState(false)
   const [usedChars, setUsedChars] = useState<string[]>([])
   const [solvedChars, setSolvedChars] = useState<string[]>([])
-  const [revealedIndexes, setRevealedIndexes] = useState<number[]>(initialRevealedIndexes)
+  const [revealedIndexes, setRevealedIndexes] = useState<number[]>(getRevealedIndexes(chars, (c) => /[^\w]/.test(c)))
 
   const handleSolve = () => {
     const indexes = chars.reduce((acc: number[], char) => {
--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,12 @@ export const isSolved = (char: string, solvedChars: string[]): boolean => {
 export const isRevealed = (char: string, solvedChars: string[]): boolean => {
   return false
 }
+
+export const getRevealedIndexes = (chars: string[], shouldReveal: (char: string) => boolean) => {
+  return chars.reduce((acc: number[], c, i) => {
+    if (shouldReveal(c)) {
+      return acc.concat(i)
+    }
+    return acc
+  }, [])
+}
