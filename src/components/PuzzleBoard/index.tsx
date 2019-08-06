68b61d12a80fcc1c4c88ddd39f8943e124044b5c
Nic Haynes <nic@nicinabox.com>
2019-08-05 21:18:53 -0500

    Adjust background, font scaling

--- src/components/PuzzleBoard/index.tsx
+++ src/components/PuzzleBoard/index.tsx
@@ -18,26 +18,28 @@ const PuzzleBoard: React.FC<Props> = ({ chars, solvedChars, revealedIndexes, onL
 
   return (
     <div className="PuzzleBoard">
-      <div className="PuzzleBoard-tiles">
-        {range(1,53).map((tileId, index) => {
-          const char = chars[index]
-          const isSpace = /[\s]/.test(char)
+      <div className="PuzzleBoard-border">
+        <div className="PuzzleBoard-tiles">
+          {range(1,53).map((tileId, index) => {
+            const char = chars[index]
+            const isSpace = /[\s]/.test(char)
 
-          return (
-            <Tile key={index} className={`Tile-${tileId}`}>
-              {char && !isSpace ? (
-                <Letter
-                  char={char}
-                  isSolved={solvedChars.includes(char)}
-                  isRevealed={revealedIndexes.includes(index)}
-                  onReveal={() => onLetterReveal(index)}
-                />
-              ) : (
-                <Logo className="Tile-logo" width="100%" height="100%" />
-              )}
-            </Tile>
-          )
-        })}
+            return (
+              <Tile key={index} className={`Tile-${tileId}`}>
+                {char && !isSpace ? (
+                  <Letter
+                    char={char}
+                    isSolved={solvedChars.includes(char)}
+                    isRevealed={revealedIndexes.includes(index)}
+                    onReveal={() => onLetterReveal(index)}
+                  />
+                ) : (
+                  <Logo className="Tile-logo" width="100%" height="100%" />
+                )}
+              </Tile>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
