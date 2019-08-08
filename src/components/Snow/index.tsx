66fbe3ce501f42dd34fdd4edcecbc6f9f8861783
Nic Haynes <nic@nicinabox.com>
2019-08-07 22:43:04 -0500

    Simplify snow
    Former-commit-id: 8f441a9d4a27f9d77d31034ff6a9cb806fe2c9f7

--- src/components/Snow/index.tsx
+++ src/components/Snow/index.tsx
@@ -2,28 +2,30 @@ import React from 'react'
 import { range, random } from 'lodash'
 import './styles.css'
 
-interface Props {}
+interface Props {
+  totalFlakes?: number;
+}
 
 const generateStyle = (n: number, total: number) => {
-  const size = random(0.1, 0.5);
-  const opacity = random(0.3, 0.9);
+  const size = random(0.1, 0.5)
+  const opacity = random(0.3, 0.9)
+  const animationDuration = random(8, 40)
+  const animationDelay = random(1, 15)
+
   return ({
     opacity,
     width: `${size}vw`,
     height: `${size}vw`,
-    animationDuration: `${random(8, 40)}s`,
-    animationDelay: `${random(1, 15)}s`,
-    left: `${n * (100 / total)}%`,
+    animationDuration: `${animationDuration}s`,
+    animationDelay: `${animationDelay}s`,
   })
 }
 
-export const Snow: React.FC<Props> = () => {
-  const totalFlakes = 100 // random(30, 60)
-
+export const Snow: React.FC<Props> = ({ totalFlakes = 100 }) => {
   return (
     <div className="Snow">
       {range(1, totalFlakes + 1).map((n, i) => (
-        <div
+        <i
           className="Snowflake"
           style={generateStyle(i, totalFlakes)}
         />
