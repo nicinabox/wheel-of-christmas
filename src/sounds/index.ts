f00daca703fc02edeb72683c81f3055c3448e45c
Nic Haynes <nic@nicinabox.com>
2019-08-07 20:42:06 -0500

    Add sounds
    Former-commit-id: 33ad6762792f43ffb96711f64b444bf49043481c

--- src/sounds/index.ts
+++ src/sounds/index.ts
@@ -1,5 +1,22 @@
+// Puzzle board sounds
 export const BUZZER = 'Buzzer.wav'
 export const DING = 'Ding.wav'
+export const LAST_SPIN = 'Last Spin.wav'
+export const NO_VOWELS_LEFT = 'No Vowels Left.wav'
 export const PUZZLE_REVEAL = 'Puzzle Reveal.mp3'
 export const PUZZLE_SOLVE = 'Puzzle Solve.wav'
-export const RSTLNE = 'RSTLNE.mp3'
+
+// Wheel sounds
+export const BANKRUPT = 'Bankrupt.wav'
+export const BEN_WEDGE = 'Ben Wedge.wav'
+export const EXPRESS = 'Express.wav'
+export const HALF_CARD = 'Half Card.wav'
+export const MYSTERY = 'Mystery.wav'
+export const WILD_CARD = 'Wild Card.wav'
+
+// Manual
+export const BONUS_ROUND_SOLVE = 'Bonus Round Solve.wav'
+export const BONUS_ROUND_TIMER = 'Bonus Round Timer.wav'
+export const THEME = 'Theme.wav'
+export const TOSS_UP_SOLVE = 'Toss Up Solve.wav'
+export const TOSS_UP_THEME = 'Toss Up Theme.wav'
