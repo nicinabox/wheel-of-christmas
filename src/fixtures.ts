a7272ffca6f201f2a6e0de0166ada47afb2219dc
Nic Haynes <nic@nicinabox.com>
2019-12-01 18:50:00 -0600

    Update fixtures
    Former-commit-id: 1f4ef611d2c901e9e67ccda42cd9e8bbde3599ba

---
+++ src/fixtures.ts
@@ -0,0 +1,29 @@
+import API from "interfaces/api";
+
+export const GAME_FIXTURE: API.Game[] = [
+  {
+    id: 1,
+    puzzles: [
+      {
+        category: 'Fictional Character',
+        phrase: "  DR.       FRANKENSTEIN'S ASSISTANT          IGOR"
+      },
+      {
+        category: 'Classic Movie',
+        phrase: '             CITIZEN KANE'
+      },
+      {
+        category: 'Song Artist',
+        phrase: '               AFRICA         BY TOTO',
+      },
+      {
+        category: 'Movie Quotes',
+        phrase: '             YOU SHALL       NOT PASS!',
+      },
+      {
+        category: "The 90's",
+        phrase: '               BEEPERS'
+      },
+    ]
+  }
+]
