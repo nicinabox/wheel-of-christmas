5b4d9f57f1cd8adb3e6d2259e8adb4209cec112f
Nic Haynes <nic@nicinabox.com>
2019-12-05 19:11:31 -0600

    Update puzzle api
    Former-commit-id: df5b53ad7c0f2740ff84497ab3369574ac9d6228

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -5,24 +5,39 @@ export const GAME_FIXTURE: API.Game[] = [
     id: 1,
     puzzles: [
       {
+        id: 1,
         category: 'fictional_character',
-        phrase: "  DR.       FRANKENSTEIN'S ASSISTANT          IGOR"
+        phrase: "  DR.       FRANKENSTEIN'S ASSISTANT          IGOR",
+        name: null,
+        bonus_round: false,
       },
       {
+        id: 2,
         category: 'classic_movie',
-        phrase: '             CITIZEN KANE'
+        phrase: '             CITIZEN KANE',
+        name: null,
+        bonus_round: false,
       },
       {
+        id: 3,
         category: 'song_artist',
         phrase: '               AFRICA         BY TOTO',
+        name: null,
+        bonus_round: false,
       },
       {
+        id: 4,
         category: 'movie_quotes',
         phrase: '             YOU SHALL       NOT PASS!',
+        name: null,
+        bonus_round: false,
       },
       {
+        id: 5,
         category: "the_90_s",
-        phrase: '               BEEPERS'
+        phrase: '               BEEPERS',
+        name: null,
+        bonus_round: false,
       },
     ]
   }
--- src/interfaces/api.ts
+++ src/interfaces/api.ts
@@ -5,8 +5,11 @@ declare namespace API {
   }
 
   export interface Puzzle {
+    id: number
     phrase: string
     category: string
+    name: string | null
+    bonus_round: boolean
   }
 }
 
