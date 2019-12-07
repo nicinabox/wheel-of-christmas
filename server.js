e74cf648cc8b5e592f8a98801dfbaf27f1c63e06
Nic Haynes <nic@nicinabox.com>
2019-12-07 12:57:16 -0600

    Add proxy
    Former-commit-id: d51d74d1a16e94132cdad8ec929b4d2b56d57f0f

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+const proxy = require('express-http-proxy')
 const express = require('express')
 const path = require('path')
 const app = express()
 
 const PORT = process.env.PORT || 9000
+const WHEEL_API_URL = process.env.WHEEL_API_URL
 
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.static(path.join(__dirname, 'static')))
+
+app.use('/api', proxy(WHEEL_API_URL))
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
