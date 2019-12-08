3a8228e7a35960a2cd37e781117e9cc688681bc3
Nic Haynes <nic@nicinabox.com>
2019-12-07 19:32:39 -0600

    Fix reload
    Former-commit-id: 916f851f1922a98b5d005cca7fc34b45c76562bc

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'static')))
 
 setupProxy(app)
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
